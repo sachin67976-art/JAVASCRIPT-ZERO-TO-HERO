3f97a1f84cf39442c34ed74ec66b9f81269d9d33
sachin67976-art <sachin67976@gmail.com>
2026-09-19 13:54:21 +0530

    variable

--- VARIABLE/variable.js
+++ VARIABLE/variable.js
@@ -1,4 +1,43 @@
-fullname ="tony stark";
+/*fullname ="tony stark";
 console.log(fullname);
 int =24;
 console.log(int);
+price =99.90;
+console.log("proce");*/
+let fullname ="sachin yadav";
+console.log(fullname);
+var totalprice=25555; //2015 me use hota tha 
+console.log(totalprice);
+const PI = 3.14;
+console.log(PI);
+{
+    let q =10;
+    console.log(q);
+}
+const student={
+    fullname: "sachin yadav",
+    age:20,
+    cgpa:7.2,
+    ispass:true
+
+
+};
+student["age"]=student["age"]+1;
+console.log(student['age']);
+
+const product={
+    title: "ball pen",
+    rating: 4,
+    offer:5,
+    price:270,
+
+};
+const profile={
+    username :" @sachinyadav",
+    isfollow: false,
+    followers: 13,
+    following :23,
+
+
+};
+console.log(typeof profile["isfollow"]);
