7927dd8cfef32eb9ce7f9872ae09f22ec2ee6b85
sachin67976-art <sachin67976@gmail.com>
2026-09-21 17:56:50 +0530

    operaating

---
+++ operating/operating.js
@@ -0,0 +1,19 @@
+// let a = 5;
+// let b = 2;
+// console.log("a+b=", a + b );
+// console.log("a-b=", a - b );
+// console.log("a=",a, "& b=", b);
+// console.log("a % b= ", a%b);
+// console.log("a**b=",a**b);
+
+//unary operator
+//  let a =5;
+//  let b =5;
+
+// console.log("a+", a++);
+// console.log("a=", a);
+// console.log("a--", a--);
+// console.log("a=", a);
+let a =5;
+ let b =5;
+ console.log("5 ! =5", a!=b);
